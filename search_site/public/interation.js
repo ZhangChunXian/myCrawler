03f8b0a0a64ef74d1fd86e0ec7c63e7d2a1ce915
Chunxian Zhang <1836891291@qq.com>
2021-04-28 21:49:45 +0800

    update 163news block

--- search_site/public/interation.js
+++ search_site/public/interation.js
@@ -1,17 +1,34 @@
 $(document).ready(function() {
+    // 页面加载完成
+    // 间事件发生
+    let i = 0;
     $("input:button").click(function() {
         $.get('/process_get?title=' + $("input:text").val(), function(data) {
-            $("#record2").empty();
-            $("#record2").append('<tr class="cardLayout"><td>url</td><td>source_name</td>' +
-                '<td>title</td><td>author</td><td>publish_date</td></tr>');
+            // 清空id为record163的表格
+            $("#record163").empty();
+            // 表格加入表头
+            $("#record163").append('<tr>' +'<th>id</th><th>title</th><th>author</th><th>publish_date</th></tr>');
+            // 表格添加值 list即新爬取的json文件
             for (let list of data) {
-                let table = '<tr class="cardLayout"><td>';
-                Object.values(list).forEach(element => {
-                    table += (element + '</td><td>');
-                });
-                $("#record2").append(table + '</td></tr>');
+                if (list.source_name == "网易新闻") {
+                    let table = '<tr><td>';
+                    Object.values(list).forEach(element => {
+                        if (element == list.title || element == list.author || element == list.publish_date) {
+                            if (element == list.title) {
+                                i++;
+                                table += (i + '</td><td><a href="' + list.url + '">' + element + '</a>');
+                                table += '</td><td>';
+                            } else if (element == list.author ) {
+                                table += (element.substring(0, element.lastIndexOf('_')) + '</td><td>');
+                            } else if (element == list.publish_date) {
+                                table += (element.substr(0, 10) + '</td>');
+                            }
+                        }
+                    });
+                    $("#record163").append (table + '</td></tr>');
+                }
+
             }
+            });
         });
     });
-
-});
