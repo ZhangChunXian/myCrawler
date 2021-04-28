0965515b5cfb707d4d941e5e5bce7fd4898c7c33
Chunxian Zhang <1836891291@qq.com>
2021-04-28 18:41:42 +0800

    完成了图形化导航栏

--- search_site/public/interation.js
+++ search_site/public/interation.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
     $("input:button").click(function() {
         $.get('/process_get?title=' + $("input:text").val(), function(data) {
             $("#record2").empty();
-            $("#record2").append('<tr class="cardLayout"><th>url</th><th>source_name</th>' +
-                '<th>title</th><th>author</th><th>publish_date</th></tr>');
+            $("#record2").append('<tr class="cardLayout"><td>url</td><td>source_name</td>' +
+                '<td>title</td><td>author</td><td>publish_date</td></tr>');
             for (let list of data) {
-                console.log(list);
                 let table = '<tr class="cardLayout"><td>';
                 Object.values(list).forEach(element => {
                     table += (element + '</td><td>');
