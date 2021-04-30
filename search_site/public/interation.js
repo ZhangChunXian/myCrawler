42b8f04c73b9dca1f7d42807246e3ada33b966b1
Chunxian Zhang <1836891291@qq.com>
2021-04-30 09:28:18 +0800

    finish kknews

--- search_site/public/interation.js
+++ search_site/public/interation.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
     // 页面加载完成
     // 间事件发生
-    let i = 0;
+    /* 网易新闻条目数 */
+    let news163Item = 0;
+    /* 看看新闻条目数 */
+    let kknewsItem = 0;
     $("input:button").click(function() {
         $.get('/process_get?title=' + $("input:text").val(), function(data) {
             // 清空id为record163的表格
             $("#record163").empty();
             // 表格加入表头
             $("#record163").append('<tr>' +'<th>id</th><th>title</th><th>author</th><th>publish_date</th></tr>');
+            // 清空id为kknewsrecord的表格
+            $("#kknewsrecord").empty();
+            // 表格加入表头
+            $("#kknewsrecord").append('<tr>' +'<th>id</th><th>title</th><th>author</th><th>publish_date</th></tr>');
             // 表格添加值 list即新爬取的json文件
             for (let list of data) {
                 if (list.source_name == "网易新闻") {
@@ -15,8 +22,8 @@ $(document).ready(function() {
                     Object.values(list).forEach(element => {
                         if (element == list.title || element == list.author || element == list.publish_date) {
                             if (element == list.title) {
-                                i++;
-                                table += (i + '</td><td><a href="' + list.url + '">' + element + '</a>');
+                                news163Item++;
+                                table += (news163Item + '</td><td><a href="' + list.url + '">' + element + '</a>');
                                 table += '</td><td>';
                             } else if (element == list.author ) {
                                 table += (element.substring(0, element.lastIndexOf('_')) + '</td><td>');
@@ -28,7 +35,25 @@ $(document).ready(function() {
                     $("#record163").append (table + '</td></tr>');
                 }
 
+                if (list.source_name == "看看新闻网国际板块") {
+                    let table = '<tr><td>';
+                    Object.values(list).forEach(element => {
+                        if (element == list.title || element == list.author || element == list.publish_date) {
+                            if (element == list.title) {
+                                kknewsItem++;
+                                table += (kknewsItem + '</td><td><a href="' + list.url + '">' + element + '</a>');
+                                table += '</td><td>';
+                            } else if (element == list.author ) {
+                                table += (element + '</td><td>');
+                            } else if (element == list.publish_date) {
+                                table += (element.substr(0, 10) + '</td>');
+                            }
+                        }
+                    });
+                    $("#kknewsrecord").append (table + '</td></tr>');
+                }
             }
+
             });
         });
     });
